ec5d05b55f2e4236df704c626b7b5bd62cf52699
Michelle Chow <40366174+michellechowdc@users.noreply.github.com>
2019-07-02 15:46:43 -0700

    Update portfolioScript.js

--- portfolioScript.js
+++ portfolioScript.js
@@ -1,41 +1,13 @@
-// change pic color when hover
-function picHoverOn(id) {
-  document.getElementById(id).className = "container blur";
-}
-
-function picHoverOff(id) {
-  document.getElementById(id).className = "container";
-}
-
-function showTitle(id) {
-  // document.getElementById(id).style.display = "block";
-  document.getElementById(id).className = "show";
-}
-
-function hideTitle(id) {
-  // document.getElementById(id).style.display = "none";
-  document.getElementById(id).className = "hide";
-}
-
-
-
-
-// new
-function displayQuote() {
-    document.getElementById("picTitle").textContent = "Project Name Title Thing";
+function displayQuote(display) {
+    document.getElementById("picTitle").className = display;
 }
 
 function showTitleAndBlur(id) {
+  displayQuote("show");
   document.getElementById(id).className = "container blur";
-  displayQuote();
-  // document.getElementById(picTitle).style.display = "block";
-  // document.getElementById(titleId).className = "show";
 }
 
 function hideTitleNoBlur(id) {
+  displayQuote("hide");
   document.getElementById(id).className = "container";
-
-  // document.getElementById(titleId).className = "hide";
-  // document.getElementById(picTitle).style.display = "none";
-
 }
